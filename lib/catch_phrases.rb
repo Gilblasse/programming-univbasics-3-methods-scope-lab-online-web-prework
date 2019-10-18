f6504780ab156e12128aad789eca7ef349eb0fb1
Nethelbert <gilblasse@gmail.com>
2019-10-18 16:06:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  toadstool(status)
   puts phrase
 end
 
-def toadstool(status)
-  puts status
+def toadstool
+  puts $status
 end
